e507b690c7012c67b7d3d539864735a3bb98892e
Deniz Gülal <denizgulal@Denizs-MacBook-Pro.local>
2023-12-03 22:35:00 +0300

    fix: constructing deck

--- src/domain/IngredientCardDeck.java
+++ src/domain/IngredientCardDeck.java
@@ -4,19 +4,15 @@ import java.util.ArrayList;
 
 public class IngredientCardDeck {
 	
-	private ArrayList<IngredientCard> ingredientCardDeck = new ArrayList<IngredientCard>(15);
+	private ArrayList<IngredientCard> ingredientCardDeck = new ArrayList<IngredientCard>();
 
+
+	public IngredientCardDeck() {
 	IngredientCard ingredientCard_1 = new Herb("ginger", 1, 3, "yellow");
 	IngredientCard ingredientCard_2 = new Herb("rosemary", 2, 5, "green");
 	IngredientCard ingredientCard_3 = new Herb("cilantro", 3, 9, "green");
 	IngredientCard ingredientCard_4 = new Herb("dill", 4, 11, "green");
 	IngredientCard ingredientCard_5 = new Herb("sage", 5, 13, "lilac");
-
-	ingredientCardDeck.add(ingredientCard_1);
-	ingredientCardDeck.add(ingredientCard_2);
-	ingredientCardDeck.add(ingredientCard_3);
-	ingredientCardDeck.add(ingredientCard_4);
-	ingredientCardDeck.add(ingredientCard_5);
 
 	IngredientCard ingredientCard_6 = new Mineral("copper", 1, 5, "grey");
 	IngredientCard ingredientCard_7 = new Mineral("magnesium", 2, 9, "white");
@@ -29,8 +25,22 @@ public class IngredientCardDeck {
 	IngredientCard ingredientCard_13 = new Mushroom("oyster", 3, 9, "white");
 	IngredientCard ingredientCard_14 = new Mushroom("hedgehog", 4, 2, "brown");
 
+	ingredientCardDeck.add(ingredientCard_1);
+	ingredientCardDeck.add(ingredientCard_2);
+	ingredientCardDeck.add(ingredientCard_3);
+	ingredientCardDeck.add(ingredientCard_4);
+	ingredientCardDeck.add(ingredientCard_5);
+	ingredientCardDeck.add(ingredientCard_6);
+	ingredientCardDeck.add(ingredientCard_7);
+	ingredientCardDeck.add(ingredientCard_8);
+	ingredientCardDeck.add(ingredientCard_9);
+	ingredientCardDeck.add(ingredientCard_10);
+	ingredientCardDeck.add(ingredientCard_11);
+	ingredientCardDeck.add(ingredientCard_12);
+	ingredientCardDeck.add(ingredientCard_13);
+	ingredientCardDeck.add(ingredientCard_14);
 
-	
+	}
 
 
 
