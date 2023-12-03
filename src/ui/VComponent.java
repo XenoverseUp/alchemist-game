139bf08c262d11467c1c450048cf48578b3290e3
XenoverseUp <xenoverseup@gmail.com>
2023-12-03 04:34:52 +0300

    misc:add JavaDoc comments

--- src/ui/VComponent.java
+++ src/ui/VComponent.java
@@ -4,6 +4,19 @@ import java.awt.Color;
 import javax.swing.JPanel;
 import interfaces.Renderable;
 
+/**
+ * @abstract
+ * 
+ * This class initializes the JPanel for rendering in the router and implements
+ * `getContentPanel` method. It also has an abstract `buildView` method that is responsible 
+ * for filling in the panel.
+ * 
+ * This abstract class is suitable for views that don't have to access `TheAlchemistGame` 
+ * game instance.
+ * 
+ * @see VStart.java
+ * 
+ */
 public abstract class VComponent implements Renderable {
     protected JPanel panel = new JPanel();
 
--- src/ui/VGameComponent.java
+++ src/ui/VGameComponent.java
@@ -2,6 +2,18 @@ package ui;
 
 import domain.TheAlchemistGame;
 
+/**
+ * @abstract
+ * 
+ * This class initializes the `TheAlchemistGame` instance. It also has an abstract `buildView` method 
+ * that is responsible for filling in the panel.
+ * 
+ * This abstract class is suitable for views that have to access `TheAlchemistGame` game instance.
+ * 
+ * @see VLogin.java
+ * 
+ */
+
 public abstract class VGameComponent extends VComponent {
     protected TheAlchemistGame game;
 
