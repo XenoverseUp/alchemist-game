7a91b789a7b832b527ca3e01ec206d06862f64bf
Deniz Gülal <denizgulal@Denizs-MacBook-Pro.local>
2023-12-03 21:44:38 +0300

    fix: class naming

--- src/domain/IngredientCard.java
+++ src/domain/IngredientCard.java
@@ -3,11 +3,35 @@ package domain;
 public class IngredientCard {
 
 	private int id;
+    private String name;
+    private String color;
+    private int value;
 
 	public int getId() {
 		return id;
 	}
 	
-	
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+	public String getColor() {
+		return color;
+	}
+	public void setColor(String color) {
+		this.color = color;
+	}
+	public int getValue() {
+		return value;
+	}
+	public void setValue(int value) {
+		this.value = value;
+	}
 
 }
