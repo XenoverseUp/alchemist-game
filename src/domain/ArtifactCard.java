3c643d03069937493bf01daede501be7376eeb4a
Deniz Gülal <denizgulal@Denizs-MacBook-Pro.local>
2023-12-03 21:47:31 +0300

    fix: artifactCard definitions

--- src/domain/ArtifactCard.java
+++ src/domain/ArtifactCard.java
@@ -1,7 +1,24 @@
 package domain;
 
-public class ArtifactCard {
-	
-	public int price;
 
+public class ArtifactCard {
+
+	private String effectType;
+	private String ability;
+
+	
+	public String getEffectType() {
+		return effectType;
+	}
+	public void setEffectType(String effectType) {
+		this.effectType = effectType;
+	}
+	public String getAbility() {
+		return ability;
+	}
+	public void setAbility(String ability) {
+		this.ability = ability;
+	}
+
+	
 }
