596c7a17f9a9fc0c20fe67749a186d8a71fe261e
Deniz Gülal <denizgulal@Denizs-MacBook-Pro.local>
2023-12-03 22:35:35 +0300

    fix: correction

--- src/domain/IngredientCard.java
+++ src/domain/IngredientCard.java
@@ -16,14 +16,6 @@ public class IngredientCard {
 
 	}
 
-
-
-	IngredientCard ingredientCard_1 = new Herb("ginger", 1, 3, "yellow");
-	IngredientCard ingredientCard_2 = new Herb("rosemary", 2, 5, "green");
-	IngredientCard ingredientCard_3 = new Herb("cilantro", 3, 9, "green");
-	IngredientCard ingredientCard_4 = new Herb("dill", 4, 11, "green");
-	IngredientCard ingredientCard_5 = new Herb("sage", 5, 13, "lilac");
-
 	
 	public void setName(String name) {
 		this.name = name;
