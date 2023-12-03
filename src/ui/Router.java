78931bb66c8d6027130e32a015100b73153cdd1d
CerenTarim <ftarim19@ku.edu.tr>
2023-12-03 17:50:02 +0300

    changed

--- src/ui/Router.java
+++ src/ui/Router.java
@@ -1,4 +1,4 @@
-package ui;
+package UI;
 
 import java.util.Map;
 import java.util.Stack;
--- src/ui/VStart.java
+++ src/ui/VStart.java
@@ -1,4 +1,4 @@
-package ui;
+package UI;
 
 import java.awt.Rectangle;
 import java.awt.image.BufferedImage;
