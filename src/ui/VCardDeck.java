13ec5b0785bb252108154c608e2d516dc454c7a0
atatutek <atutek20@ku.edu.tr>
2023-12-05 22:01:52 +0300

    Card Deck UI elements added.

--- src/ui/VCardDeck.java
+++ src/ui/VCardDeck.java
@@ -6,34 +6,56 @@ import javax.swing.Box;
 import javax.swing.BoxLayout;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
+import javax.swing.BoxLayout;
+import enums.View;
 
 import domain.TheAlchemistGame;
 
 public class VCardDeck extends VGameComponent {
     private Router router = Router.getInstance();
-    
-    public VCardDeck(TheAlchemistGame game) { super(game); }
+
+    public VCardDeck(TheAlchemistGame game) {
+        super(game);
+    }
 
     @Override
     protected void render() {
-        panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
+        JPanel ingredientCardDeck = createCardDeck("Ingredient Card Deck");
+        JPanel artifactCardDeck = createCardDeck("Artifact Card Deck");
 
-        JLabel text = new JLabel("Card Deck");
-        text.setAlignmentX(Component.CENTER_ALIGNMENT);
+        ingredientCardDeck.setBounds(0, 0, Window.frame.getWidth() / 2, Window.frame.getHeight());
+        artifactCardDeck.setBounds(Window.frame.getWidth() / 2, 0, Window.frame.getWidth() / 2,
+                Window.frame.getHeight());
 
-        
-        
-        JButton back = new JButton("Back");
-        back.setAlignmentX(Component.CENTER_ALIGNMENT);
-        back.addActionListener(event -> {
-            router.navigateBack();
+        panel.add(ingredientCardDeck);
+        panel.add(artifactCardDeck);
+    }
+
+    private JPanel createCardDeck(String t) {
+        JPanel cardPanel = new JPanel();
+        cardPanel.setLayout(new BoxLayout(cardPanel, BoxLayout.Y_AXIS));
+
+        JLabel title = new JLabel(t);
+        title.setAlignmentX(Component.CENTER_ALIGNMENT);
+
+        JButton button = new JButton("Draw");
+        button.setAlignmentX(Component.CENTER_ALIGNMENT);
+        button.addActionListener(event -> {
+            // add card to current user
+            router.to(View.Board);
         });
-        
-        panel.add(Box.createVerticalGlue());
-        panel.add(text);
-        panel.add(Box.createVerticalStrut(10));
-        panel.add(back);
-        panel.add(Box.createVerticalGlue());
+
+        cardPanel.add(Box.createVerticalGlue());
+        cardPanel.add(title);
+        cardPanel.add(Box.createVerticalStrut(24));
+        // Card Deck
+        cardPanel.add(Box.createVerticalStrut(24));
+        cardPanel.add(button);
+        cardPanel.add(Box.createVerticalGlue());
+
+        return cardPanel;
+
     }
 
 }
