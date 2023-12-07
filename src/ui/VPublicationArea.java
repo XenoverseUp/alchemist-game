0b0c37341773775f86594a841ebf92b7a60f4880
bbilici20 <bbilici20@ku.edu.tr>
2023-12-07 03:30:32 +0300

    fu-pba: publication area first implementation

--- src/ui/VPublicationArea.java
+++ src/ui/VPublicationArea.java
@@ -1,39 +1,68 @@
 package ui;
 
 import java.awt.Component;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
-import javax.swing.Box;
-import javax.swing.BoxLayout;
+import javax.imageio.ImageIO;
+import javax.swing.ImageIcon;
 import javax.swing.JButton;
 import javax.swing.JLabel;
+import javax.swing.JPanel;
 
+import enums.View;
 import domain.TheAlchemistGame;
 
 public class VPublicationArea extends VComponent {
     private Router router = Router.getInstance();
 
-    public VPublicationArea(TheAlchemistGame game) { super(game); }
+    public VPublicationArea(TheAlchemistGame game) {
+        super(game);
+    }
 
     @Override
     protected void render() {
-         panel.setLayout(new BoxLayout(panel, BoxLayout.Y_AXIS));
-
+        JPanel publicationPanel = createPublicationJPanel();
         JLabel text = new JLabel("Publication Area");
-        text.setAlignmentX(Component.CENTER_ALIGNMENT);
+        text.setBounds(500, 10, 200, 30);
 
-        
-        
-        JButton back = new JButton("Back");
-        back.setAlignmentX(Component.CENTER_ALIGNMENT);
-        back.addActionListener(event -> {
-            router.navigateBack();
-        });
-        
-        panel.add(Box.createVerticalGlue());
+        BufferedImage bg = null;
+        try {
+            bg = ImageIO.read(new File("./src/resources/image/fantastic-library-bg.jpg"));
+        } catch (IOException e) {
+            System.out.println(e);
+        }
+
+        JLabel bgPic = new JLabel(new ImageIcon(bg));
+        bgPic.setBounds(-200, -50, Window.frame.getWidth(), Window.frame.getHeight());
+
+        panel.setLayout(null); // Use absolute positioning
+        panel.add(publicationPanel);
         panel.add(text);
-        panel.add(Box.createVerticalStrut(10));
-        panel.add(back);
-        panel.add(Box.createVerticalGlue());
+        panel.add(bgPic);
+    }
+
+    private JPanel createPublicationJPanel() {
+        JPanel publicationPanel = new JPanel();
+        publicationPanel.setBounds(0, 0, Window.frame.getWidth(), Window.frame.getHeight());
+        publicationPanel.setLayout(null); // Use absolute positioning
+        publicationPanel.setOpaque(false);
+
+        JButton back = new JButton("Back");
+        back.setBounds(480, 650, 150, 30);
+        back.addActionListener(event -> router.to(View.Board));
+
+        JButton debunkTheory = new JButton("Display Available Publication Cards");
+        debunkTheory.setBounds(400, 370, 300, 30);
+        debunkTheory.addActionListener(event -> {
+            router.to(View.DeductionBoard);
+        });
+
+        publicationPanel.add(back);
+        publicationPanel.add(debunkTheory);
+
+        return publicationPanel;
     }
 
 }
