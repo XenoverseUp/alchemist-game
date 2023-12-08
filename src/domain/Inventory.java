fd02d1345095087cec40735d36b49f7a52285444
bbilici20 <bbilici20@ku.edu.tr>
2023-12-08 22:52:23 +0300

    fui-use-artifacts: remove artifact card function added

--- src/domain/Inventory.java
+++ src/domain/Inventory.java
@@ -2,26 +2,24 @@ package domain;
 
 import java.util.ArrayList;
 
-
 public class Inventory {
-	
+
 	private int gold = 0;
 	private ArrayList<ArtifactCard> artifactCards = new ArrayList<>();
 	private ArrayList<IngredientCard> ingredientCards = new ArrayList<>();
-	
-	
+
 	public void addIngredientCard(IngredientCard ingredient) {
 		this.ingredientCards.add(ingredient);
 	}
-	
+
 	public void addArtifactCard(ArtifactCard artifact) {
 		this.artifactCards.add(artifact);
 	}
-	
-	public void addGold (int amount) {
+
+	public void addGold(int amount) {
 		this.gold += amount;
 	}
-	
+
 	public void removeGold(int amount) {
 		this.gold -= amount;
 	}
@@ -29,23 +27,42 @@ public class Inventory {
 	public ArrayList<IngredientCard> getIngredientCards() {
 		return ingredientCards;
 	}
-	
+
 	public ArrayList<ArtifactCard> getArtifactCards() {
 		return artifactCards;
 	}
 
 	public IngredientCard getIngredient(int ingredientId) {
-		for (int i = 0; i < ingredientCards.size(); i++) 
-			if (ingredientCards.get(i).getId() == ingredientId) 
+		for (int i = 0; i < ingredientCards.size(); i++)
+			if (ingredientCards.get(i).getId() == ingredientId)
 				return ingredientCards.remove(i);
-		
+
 		return null;
+		IngredientCard iCard = null;
+
+		for (IngredientCard i : ingredientCards) {
+			if (i.getId() == ingredientId) {
+				iCard = this.ingredientCards.remove(ingredientCards.indexOf(i));
+			}
+		}
+
+		return iCard;
+	}
+
+	public ArtifactCard removeArtifactCard(String name) {
+		ArtifactCard aCard = null;
+
+		for (ArtifactCard a : artifactCards) {
+			if (a.getName().equals(name)) {
+				aCard = this.artifactCards.remove(artifactCards.indexOf(a));
+			}
+		}
+
+		return aCard;
 	}
 
 	public int getGold() {
 		return gold;
 	}
 
-	
-	
 }
