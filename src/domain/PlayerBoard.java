854a9114fb48baa30e06493740bf8c742dd4e806
Digdem Yildiz <digdemyildiz19@ku.edu.tr>
2023-12-18 23:56:54 +0300

    added code lines previously not saved

--- src/domain/PlayerBoard.java
+++ src/domain/PlayerBoard.java
@@ -1,12 +1,11 @@
 package domain;
 
 import java.util.HashSet;
-import java.util.Set;
 import enums.Potion;
 
 public class PlayerBoard {
 	
-	protected Set<Potion> discoveredPotions;
+	protected HashSet<Potion> discoveredPotions;
 
 	public PlayerBoard() {
 		this.discoveredPotions = new HashSet<>();
@@ -16,6 +15,10 @@ public class PlayerBoard {
 		discoveredPotions.add(po);
 	}
 	
+	protected HashSet<Potion> getDiscoveredPotions(){
+		
+		return (HashSet<Potion>) discoveredPotions.clone();
+	}
 	
 
 }
