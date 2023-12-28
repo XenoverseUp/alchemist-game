96ab21a12fe1f04ce8ece73310c4928d0aa09c7d
Deniz Gülal <denizgulal@Denizs-MacBook-Pro.local>
2023-12-28 12:20:26 +0300

    Domain (Fix): Ingredient Card Deck

--- src/domain/IngredientCardDeck.java
+++ src/domain/IngredientCardDeck.java
@@ -6,6 +6,7 @@ import java.util.Collections;
 
 public class IngredientCardDeck {
 	private ArrayList<IngredientCard> ingredientCardDeck = new ArrayList<IngredientCard>();
+	private Integer index = -1;
 	private ArrayList<String> ingredientNames = new ArrayList<String>() {{
 		add("scorpion tail");
 		add("bird claw");
@@ -27,10 +28,33 @@ public class IngredientCardDeck {
 	public void shuffle() {
 		Collections.shuffle(ingredientCardDeck);		
 	}
-	
+
+
+
+
+
+	/*-------------------------------------------------------------------------------------------------------------------------
+	 * We keep an index to avoid getting the same ingredient card from the deck and the problem of having zero cards in the end.
+	 * First we initialize it as zero as a private variable in the class.
+	 * In the first card drawing we increment it and draw the 0th placed card in the ingredientCardDeck Array.
+	 * If we reach at the end of the Array, then we reset the index, shuffle the deck and return the last card.
+	 */
+
+
 	public IngredientCard drawCard() {
-		IngredientCard iCard = ingredientCardDeck.remove(ingredientCardDeck.size() - 1);
-		return iCard;	
+		Integer indx = incrIndex();
+		//IngredientCard iCard = ingredientCardDeck.remove(ingredientCardDeck.size() - 1);
+		if (indx == ingredientCardDeck.size()){
+			indx = -1;
+			shuffle();
+			IngredientCard iCard = ingredientCardDeck.get(ingredientCardDeck.size() - 1);
+			return iCard;
+		}
+		else {
+			incrIndex();
+			IngredientCard iCard = ingredientCardDeck.get(indx);
+			return iCard;	
+		}
 	}
 
 	public IngredientCard getByName(String name) {
@@ -46,8 +70,10 @@ public class IngredientCardDeck {
 		ingredientCardDeck.add(card);
 	}
 	
+	public Integer incrIndex(){
+		return index++;
+	}
 	
-	
-	
+
 
 }
