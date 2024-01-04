db4744e6481f72d81e4c31d0a3d9378cb68362a2
Deniz Gülal <denizgulal@Denizs-MacBook-Pro.local>
2024-01-04 12:28:31 +0300

    fix:imports

--- src/domain/test/PublicationCardDeckTest.java
+++ src/domain/test/PublicationCardDeckTest.java
@@ -1,6 +1,7 @@
+package domain.test;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.Assert.*;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertThrows;
 import static org.junit.Assert.assertTrue;
 
 
