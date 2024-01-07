cccdae23b3ed00a0da5de2e47837c9e3a4c47596
Deniz Gülal <denizgulal@Denizs-MacBook-Pro.local>
2024-01-07 19:51:50 +0300

    fix:comments

--- src/test/domain/PublicationCardDeckTest.java
+++ src/test/domain/PublicationCardDeckTest.java
@@ -23,6 +23,9 @@ public class PublicationCardDeckTest {
         cardDeck.shuffle();
     }
 
+
+    //Black-box test since it tests the functionality without knowing the internal implementation. 
+    //It verifies that drawing a card reduces the deck size by one.
     @Test
     public void testDrawCardRemovesOneCard() {
         int initialSize = cardDeck.getPublicationCardDeck().size();
@@ -30,12 +33,18 @@ public class PublicationCardDeckTest {
         assertEquals(initialSize - 1, cardDeck.getPublicationCardDeck().size());
     }
 
+
+    //bBack-box test as it checks if the drawn card is an instance of PublicationCard 
+    //without requiring knowledge of the internal implementation.
     @Test
     public void testDrawCardReturnsPublicationCard() {
         PublicationCard drawnCard = cardDeck.drawCard();
         assertTrue(drawnCard instanceof PublicationCard);
     }
 
+
+    //Black-box test as it checks the behavior of the shuffle() method, 
+    //ensuring the deck size remains the same after shuffling.
     @Test
     public void testShuffleDoesNotChangeDeckSize() {
         int initialSize = cardDeck.getPublicationCardDeck().size();
@@ -44,7 +53,8 @@ public class PublicationCardDeckTest {
         assertEquals("Shuffling should not change the deck size", initialSize, shuffledSize);
     }
     
-
+    //Glass-box test as it examines the behavior of the method by directly 
+    //controlling the loop based on the deck size and checking if the deck size becomes zero after drawing all cards.
     @Test
     public void testDrawAllCardsInDeck() {
         int deckSize = cardDeck.getPublicationCardDeck().size();
@@ -56,6 +66,11 @@ public class PublicationCardDeckTest {
         assertEquals(0, cardDeck.getPublicationCardDeck().size());
     }
 
+
+
+    //Both Black-box and Glass-box test. 
+    //It validates that drawn cards are unique by using a list to keep track of the drawn cards, 
+    //and it verifies the uniqueness without delving into the internal implementation.
     @Test
     public void testDrawnCardsUnique() {
         int deckSize = cardDeck.getPublicationCardDeck().size();
