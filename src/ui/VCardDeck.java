563b30d5f1b048c0a0bed501c6ff565aa0931499
XenoverseUp <xenoverseup@gmail.com>
2024-01-10 15:33:12 +0300

    chore:remove unnecessary imports

--- src/ui/VCardDeck.java
+++ src/ui/VCardDeck.java
@@ -7,7 +7,6 @@ import java.awt.Font;
 import java.awt.Image;
 import java.awt.image.BufferedImage;
 import java.io.File;
-import java.io.IOException;
 
 import javax.imageio.ImageIO;
 import javax.swing.Box;
